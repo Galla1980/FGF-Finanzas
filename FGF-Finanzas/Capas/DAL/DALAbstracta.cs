d3812b3428615fff783bfd71455fbdb28ce36d8e
Lucas Gallardo <lucasgalla1980@gmail.com>
2026-04-26 17:43:19 -0300

    Configuracion de capas y base de datos

---
+++ FGF-Finanzas/Capas/DAL/DALAbstracta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace FGF_Finanzas.Capas.DAL
+{
+    public abstract class DALAbstracta
+    {
+        private string _conexion = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+    }
+}
